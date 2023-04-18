2590df79ab8ff465ac2a8db9f0538c67f801c957
krishnapradeep5 <66884698+krishnapradeep5@users.noreply.github.com>
2023-04-18 18:47:07 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,19 +52,19 @@ const sr = ScrollReveal({
 /*SCROLL HOME*/
 sr.reveal('.home__title', {})
 sr.reveal('.home__scroll', {delay: 200})
-sr.reveal('.home__img', {origin:'right', delay: 400})
+sr.reveal('.home__img', {origin:'right', delay: 250})
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
-sr.reveal('.about__subtitle', {delay: 300})
-sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
-sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+sr.reveal('.about__img', {delay: 300})
+sr.reveal('.about__subtitle', {delay: 250})
+sr.reveal('.about__profession', {delay: 300})
+sr.reveal('.about__text', {delay: 300})
+sr.reveal('.about__social-icon', {delay: 350, interval: 200})
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 400})
+sr.reveal('.skills__img', {delay: 250})
 
 /*SCROLL PORTFOLIO*/
 sr.reveal('.portfolio__img', {interval: 200})
@@ -72,8 +72,8 @@ sr.reveal('.portfolio__img', {interval: 200})
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval: 200})
-sr.reveal('.contact__input', {delay: 400})
-sr.reveal('.contact__button', {delay: 600})
+sr.reveal('.contact__input', {delay: 250})
+sr.reveal('.contact__button', {delay: 300})
 
 
 
